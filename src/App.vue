32db14005910f717a8404d9a9a18f2010b1aaeb6
suraj200981 <suraj200981@yahoo.co.uk>
2021-11-03 17:50:16 +0000

    vuetify added and tab component added

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app> 
+ <v-app-bar
+      color="deep-purple"
+      dark
+    >
+
+      <v-toolbar-title>TodoAPP v1 by Suraj Sharma</v-toolbar-title>
+
+    </v-app-bar>
+
+    <v-main>
+      <br>
+      <center>
+      <mainForms/>
+      </center>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import mainForms from './components/mainForms';
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
-}
+    mainForms,
+  },
+
+  data: () => ({
+    //
+  }),
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
---
+++ src/components/mainForms.vue
@@ -0,0 +1,54 @@
+  <template>
+  <v-card color="basil" style="width:50%">
+    <v-card-title class="text-center justify-center py-6">
+      <h2 class="font-weight-bold text-h3 basil--text">
+        What would you like to do?
+      </h2>
+    </v-card-title>
+
+    <v-tabs
+      v-model="tab"
+      background-color="transparent"
+      color="basil"
+      grow
+    >
+      <v-tab
+        v-for="item in items"
+        :key="item"
+      >
+        {{ item }}
+      </v-tab>
+    </v-tabs>
+
+    <v-tabs-items v-model="tab">
+      <v-tab-item
+        v-for="item in items"
+        :key="item"
+      >
+        <v-card
+          color="basil"
+          flat
+        >
+          <v-card-text>{{ text }}</v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs-items>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    name: 'mainForms',
+
+    data ()  {
+      return {
+        tab: null,
+        items: [
+          'Appetizers', 'Entrees', 'Deserts', 'Cocktails',
+        ],
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      }
+    }
+  }
+   
+</script>
